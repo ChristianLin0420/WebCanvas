63e7aa05e130a8c47c5fad53e7d0244f1e7d755c
BJLin <crlc112358@gmail.com>
2020-04-04 22:22:36 +0800

    local update: set button image and added a switch to control the content mode of the tools

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+var ToolsSize = 50;
+var isFill = true;
 
+// Control whehter the element is filled or empty
+function ContentMode() {
+    isFill = !isFill;
+
+    if (isFill) {
+        document.getElementById('fill').style.color =  "#28a5e4";
+        document.getElementById('empty').style.color =  "#6f6f6f";
+    } else {
+        document.getElementById('fill').style.color =  "#6f6f6f";
+        document.getElementById('empty').style.color =  "#28a5e4";
+    }
+}
 
 // Control the size of brush and eraser
 function widthControl(value) {
-    document.getElementById('rangeValue').innerHTML = value;
+    document.getElementById('BrushRangeValue').innerHTML = value;
+    ToolsSize = value;
+    console.log(ToolsSize);
 }
+
